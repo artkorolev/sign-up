0eea5e32caaf4dc08acdea0aaea3bf8b3ed1771f
Artem Korolev <itemkorolev@gmail.com>
2022-02-15 21:53:00 +0200

    Animation images with GSAP

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
+gsap.to('img.logo', {
+    duration: 10, 
+    rotationX: 90, 
+    x: 380, 
+    opacity: 0, 
+    scale: 0.5, 
+    repeat: -1,
+});
 
-
+gsap.to('img.illustr-img', {
+    duration: 10,
+    x: 30,
+    repeat: -1
+});
