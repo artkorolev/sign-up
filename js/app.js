f5fda687bd99d2658386b2dfbd7ede84d4a3f038
Artem Korolev <itemkorolev@gmail.com>
2022-03-09 23:10:59 +0200

    Show password checked

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ gsap.to('img.illustr-img', {
     repeat: -1,
     rotation: 360
 });
+
+function showPass() {
+    const pass = document.getElementById('user-pass');
+    if (pass.type === 'password') {
+        pass.type = 'text';
+    } else pass.type ='password';
+}
